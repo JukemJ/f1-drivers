61eb643cb567057483ab28946a41fbe51e47bad1
Jukem <chris@dixn.dev>
2023-06-07 15:01:09 -0700

    adding fetch

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 
-const req = new XMLHttpRequest()
-req.addEventListener("load",function(){
-    data = this.responseXML
-    for(let race of data.getElementsByTagName('Driver')) console.log(race)
-    //console.log(data.getElementsByTagName('Race'))
-    //console.log(this.responseText)
-    test()
-
-})
-req.open("GET", "http://ergast.com/api/f1/2008/drivers")
-req.send()
-
-function test(){
-    document.getElementById('test').innerText = data
+//const data
+async function getXMLData(){
+    const response = await fetch("http://ergast.com/api/f1/2008/drivers")
+    let parser = new DOMParser()
+    const xml = parser.parseFromString(await response.text(),"application/xml")
+    console.log(xml)
 }
+getXMLData()
